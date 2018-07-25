b50a4550dbde93f88e1365d08d225f63d89b34e9
wo1176765282 <1176765282@qq.com>
2018-07-25 18:23:34 +0800

    3add

--- js/index.js
+++ js/index.js
@@ -107,13 +107,229 @@ window.onload=function () {
     }
 
 
+//banner轮播图部分
+   /* let banner_bg_lis=document.querySelectorAll('.banner_bg li');
+    console.log(banner_bg_lis);*/
+    let banner_bg=document.getElementsByClassName('banner_bg')[0];
+    let banner_bg_lis=banner_bg.getElementsByTagName('li');
+    let num=0;
+    let btnS=document.querySelectorAll('.circle li')
+    /*let circle=document.getElementsByClassName('circle')[0];
+    let btnS=circle.getElementsByTagName('li');*/
+
+    let time=setInterval(lbt,2000);
+    function lbt() {
+        num++;
+        if (num==banner_bg_lis.length){
+            num=0;
+        }
+        for (let i=0;i<banner_bg_lis.length;i++){
+            banner_bg_lis[i].style.zIndex='5';
+            btnS[i].style.background="";
+        }
+        banner_bg_lis[num].style.zIndex='9';
+        btnS[num].style.background='#fff';
+    }
+    function lbt1() {
+        num--;
+        if (num==-1){
+            num=banner_bg_lis.length-1;
+        }
+        for (let i=0;i<banner_bg_lis.length;i++){
+            banner_bg_lis[i].style.zIndex='5';
+            btnS[i].style.background="";
+        }
+
+        banner_bg_lis[num].style.zIndex='9';
+        btnS[num].style.background='#fff';
+    }
+
+    let btn_left=document.getElementsByClassName('btn_left')[0];
+    let btn_right=document.getElementsByClassName('btn_right')[0];
+    // console.log(btn_left, btn_right);
+    btn_right.onclick=function () {
+        lbt();
+        clearInterval(time);
+        time=setInterval(lbt,2000);
+    }
+    btn_left.onclick=function () {
+        lbt1();
+        clearInterval(time);
+        time=setInterval(lbt1,2000);
+    }
+
+
+
+    banner_bg.onmouseenter=function () {
+        clearInterval(time);
+    }
+    banner_bg.onmouseleave=function () {
+        time=setInterval(lbt,2000);
+    }
+
+
+        //点击圆点
+     for (let i=0;i<btnS.length;i++){
+            btnS[i].onclick=function () {
+                for (let j=0;j<btnS.length;j++){
+                    banner_bg_lis[j].style.zIndex='5';
+                    btnS[j].style.background='';
+                }
+                banner_bg_lis[i].style.zIndex='9';
+                btnS[i].style.background='#fff';
+                num=i;
+            }
+     }
 
 
 
 
+        //内容二维下标轮播
+    doubleBanner(0);
+    doubleBanner(1);
+    doubleBanner(2);
+    doubleBanner(3);
+
+    function doubleBanner(a) {
+        let contentAd = document.getElementsByClassName('contentAd')[a];
+        console.log(contentAd);
+        // let contentAd=document.getElementsByClassName('contentAd')[0];
+        let product_a = contentAd.querySelectorAll('a');
+        let now = 0;
+        let next = 0;
+        let width = parseInt(getComputedStyle(product_a[now], null).width);
+        let content_yuan = contentAd.querySelector('.content_yuan');
+        let btns = content_yuan.querySelectorAll('span');
+        let san_left = contentAd.querySelector('.san_left');
+        let san_right = contentAd.querySelector('.san_right');
+        console.log(san_right, san_left);
+        console.log(btns);
+        // let t = setInterval(move, 1000);
+        //标记
+        let flag = true;
+
+        function move() {
+            //右->左
+            next++;
+            if (next == product_a.length) {
+                next = 0;
+            }
+            //就位
+            product_a[next].style.left = width + 'px';
+            //移动
+            animate(product_a[now], {left: -width});
+            btns[now].style.border = "none";
+            animate(product_a[next], {left: 0}, function () {
+                flag = true;
+            });
+            btns[next].style.border = "2px solid #ff6700";
+            now = next;
+        }
+
+        function move1() {
+
+            //右->左
+            if (next == 0) {
+                next = product_a.length;
+            }
+            next--;
+            //就位
+            product_a[next].style.left = -width + 'px';
+            //移动
+            animate(product_a[now], {left: width});
+            btns[now].style.border = "none";
+            animate(product_a[next], {left: 0}, function () {
+                flag = true;
+            });
+            btns[next].style.border = "2px solid #ff6700";
+            now = next;
+        }
+
+        san_left.onclick = function () {
+            if (!flag) {    //flag==false
+                return;
+            }
+            if (next == 0) {
+                return;
+            }
+            flag = false;
+            move1();
+        }
+        san_right.onclick = function () {
+            if (!flag) {    //flag==false
+                return;
+            }
+            if (next == product_a.length - 1) {
+                return;
+            }
+            flag = false;
+            move();
+        }
+
+       /* contentAd.onmouseenter = function () {
+            clearInterval(t);
+        }
+        contentAd.onmouseleave = function () {
+            t = setInterval(move, 1000);
+        }*/
+        //小点点击
+        for (let i = 0; i < product_a.length; i++) {
+            btns[i].onclick = function () {
+                if (i == now) {
+                    return;
+                }
+                if (i < now) {
+                    //就位
+                    product_a[i].style.left = -width + 'px';
+                    //移动
+                    animate(product_a[i], {left: 0});
+                    btns[i].style.border = "2px solid #ff6700";
+                    animate(product_a[now], {left: width});
+                    btns[now].style.border = "none";
+                    next = now = i;
+                }
+                if (i > now) {
+                    btns[now].style.border = "none";
+                    product_a[i].style.left = width + 'px';
+                    animate(product_a[now], {left: -width});
+                    animate(product_a[i], {left: 0});
+                    btns[i].style.border = "2px solid #ff6700";
+                    next = now = i;
+                }
+            }
+        }
+
+    }
 
 
-
+    //小米闪购
+    let str_text_right=document.getElementById('str_text_right');
+    let str_text_right_a=str_text_right.querySelectorAll('a');
+    console.log(str_text_right_a);
+    let lites=document.querySelector('.listUl ul');
+    /*let litesLi=document.querySelectorAll('.listUl li');
+    console.log(litesLi);*/
+    let litesWidth=parseInt(getComputedStyle(lites,null).width)/2;
+    console.log(litesWidth);
+    console.log(lites);
+    let times=0;
+    str_text_right_a[1].onclick=function () {
+        console.log(1);
+        times++;
+        if (times==2){
+            times=1;
+        }
+        lites.style.transform=`translateX(-${litesWidth*times}px)`;
+        // console.log(times);
+        }
+    str_text_right_a[0].onclick=function () {
+        times--;
+        if (times<0){
+            times=0;
+        }
+        lites.style.transform=`translateX(-${litesWidth*times}px)`;
+        // console.log(times);
+        }
 
 
 
